ef5e82b2bcfe13d1b3408dad4226e67ba10c9f69
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2019-01-07 19:10:15 +0100

    Input quiz title, list questions

--- web/src/components/admin/Create.vue
+++ web/src/components/admin/Create.vue
@@ -1,10 +1,43 @@
 <template>
-  <h1>Create a new quiz</h1>
+  <div>
+    <h1>Create a new quiz</h1>
+    <h2 v-if="isNamed">{{name}}</h2>
+    <span v-else>
+      <label for="quizName">Enter the name of the quiz:&nbsp;</label>
+      <input id="quizName" type="text" v-model="name">
+      <button v-on:click="nameQuiz">Create</button>
+    </span>
+    <ol>
+      <li v-for="question in questions">{{question.q}} - {{question.a}}</li>
+    </ol>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'CreateQuiz'
+  name: 'CreateQuiz',
+  data() {
+    return {
+      name: "",
+      questions: [
+      {
+        q: "Who is Luke's father?",
+        a: "Darth Vader"
+      },
+      {
+        q: "What planet lies at your bottom?",
+        a: "Your anus"
+      }
+    ],
+      isNamed: false
+    }
+  },
+  methods: {
+    nameQuiz() {
+      console.log("Create a new quiz named " + this.name);
+      this.isNamed = true;
+    }
+  }
 }
 </script>
 
