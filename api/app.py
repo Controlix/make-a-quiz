30f47159bcea54e707857be190b296d6fd0e4cf1
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2019-01-17 07:42:08 +0100

    Add questions to quiz

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, json, request
 from flask_cors import cross_origin
+import pymongo
 
 app = Flask(__name__)
 
@@ -8,12 +9,25 @@ class Quiz:
         self.name = name
         self.questions = []
 
+    def add(self, question):
+        self.questions.append(question)
+
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+toJSON = lambda o: o.__dict__
+
 quizes = [Quiz("A New Quiz"), Quiz("The Return of the Chokotofs")]
+q1 = Question("Where do pinguins live?", "On the south pole.")
+quizes[0].add(q1)
+
 
 @app.route("/quiz")
 @cross_origin()
 def all_quizes():
-    return json.dumps(quizes, default=lambda o: o.__dict__)
+    return json.dumps(quizes, default=toJSON)
 
 @app.route("/quiz", methods=['POST'])
 @cross_origin()
