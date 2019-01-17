94c9c8a24ace2c864784dada05bc7e473109d38a
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2019-01-17 07:27:51 +0100

    Use mongoengine to retrieve all quizes

--- api/app.py
+++ api/app.py
@@ -1,22 +1,34 @@
 from flask import Flask, json, request
 from flask_cors import cross_origin
+from mongoengine import *
 
 app = Flask(__name__)
+connect('make_a_quiz', username='root', password='example', authentication_source='admin')
 
-class Quiz:
-    def __init__(self, name):
-        self.name = name
-        self.questions = []
+class Quiz(Document):
+    name = StringField(required = True)
+    questions = ListField(StringField())
 
-quizes = [Quiz("A New Quiz"), Quiz("The Return of the Chokotofs")]
+quizes = [Quiz(name = "A New Quiz"), Quiz(name = "The Return of the Chokotofs")]
+toJSON = lambda o: o.__dict__
 
 @app.route("/quiz")
 @cross_origin()
 def all_quizes():
-    return json.dumps(quizes, default=lambda o: o.__dict__)
+    quizes = Quiz.objects
+    for quiz in quizes:
+        print('============================')
+        print(quiz.id)
+        print(quiz.name)
+        print(quiz.questions)
+    return quizes
+    # return json.dumps(quizes, default=toJSON)
 
 @app.route("/quiz", methods=['POST'])
 @cross_origin()
 def new_quiz():
-    print(request.get_json())
+    quiz = request.get_json()
+    print(quiz)
+    savedQuiz = Quiz(name=quiz['name']).save()
+    print(savedQuiz.id)
     return json.dumps({ 'msg': 'Created' })
