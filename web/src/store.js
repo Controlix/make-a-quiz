95cd8f85c059861b1ea1f9ddb321c3863a9b155c
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2019-01-27 13:57:15 +0100

    re-send original request after token refresh

--- web/src/store.js
+++ web/src/store.js
@@ -13,7 +13,8 @@ const vuexLocal = new VuexPersistence({
 export const store = new Vuex.Store({
   state: {
     access_token: '',
-    refresh_token: ''
+    refresh_token: '',
+    status: 'UNKNOWN'
   },
   getters: {
     access_token: state => state.access_token,
@@ -23,6 +24,17 @@ export const store = new Vuex.Store({
     authenticate(state, tokens) {
       state.access_token = tokens.access_token;
       state.refresh_token = tokens.refresh_token;
+      state.status = 'LOGGED_IN';
+    },
+    refresh(state, tokens) {
+      state.access_token = tokens.access_token;
+      state.status = 'LOGGED_IN';
+    },
+    start_refresh(state) {
+      state.status = 'REFRESHING';
+    },
+    logout(state) {
+      state.status = 'UNKNOWN';
     }
   },
   plugins: [vuexLocal.plugin]
@@ -30,13 +42,24 @@ export const store = new Vuex.Store({
 
 
 Vue.http.interceptors.push((req, next) => {
-  console.log('Call ', req);
-  if (store.state.access_token) {
+  if (store.state.status === 'LOGGED_IN') {
     req.headers.set('Authorization', 'Bearer ' + store.state.access_token);
   }
+  if (store.state.status === 'REFRESHING') {
+    req.headers.set('Authorization', 'Bearer ' + store.state.refresh_token);
+  }
   next((resp, err) => {
-    if (resp.status === 401 && resp.body.msg === 'Token has expired') {
-      console.log('Must refresh access token');
+    if (resp.status === 401) {
+      if (store.state.status === 'LOGGED_IN' && resp.body.msg === 'Token has expired') {
+        store.commit('start_refresh');
+        return Promise.resolve(
+          Vue.http.get('http://localhost:5000/refresh').then(
+            resp => { store.commit('refresh', resp.body) },
+            resp => {  console.log('Something went wront', resp) }
+          ).then(_ => Vue.http(req)));
+      } else {
+        store.commit('logout');
+      }
     }
   })
 })
