e7fd4e784ae1f5242d7bcd16198f5e1049169ae5
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2019-01-27 09:33:31 +0100

    persist store to local storage

--- web/src/store.js
+++ web/src/store.js
@@ -1,20 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import jwt from 'jsonwebtoken'
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  key: 'make_a_quiz',
+  storage: window.localStorage
+})
 
 export const store = new Vuex.Store({
   state: {
     access_token: '',
     refresh_token: ''
   },
+  getters: {
+    access_token: state => state.access_token,
+    refresh_token: state => state.refresh_token
+  },
   mutations: {
     authenticate(state, tokens) {
       state.access_token = tokens.access_token;
       state.refresh_token = tokens.refresh_token;
     }
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
 
 
@@ -22,11 +33,10 @@ Vue.http.interceptors.push((req, next) => {
   console.log('Call ', req);
   if (store.state.access_token) {
     req.headers.set('Authorization', 'Bearer ' + store.state.access_token);
-    const decoded = jwt.decode(store.state.access_token);
-    if (decoded && decoded.exp < new Date()/1000) {
-      console.log('Access Token expired', decoded.exp);
-      console.log(new Date(decoded.exp * 1000));
-    }
   }
-  next();
+  next((resp, err) => {
+    if (resp.status === 401 && resp.body.msg === 'Token has expired') {
+      console.log('Must refresh access token');
+    }
+  })
 })
